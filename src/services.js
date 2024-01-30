1e9397d148f3f6bf613ed8320f31114abe3573fe
TrianaSolerMartin <triana287@gmail.com>
2024-01-30 09:19:46 +0100

    adding get

--- src/services.js
+++ src/services.js
@@ -1,24 +1,24 @@
 //Método GET R(READ) del CRUD
 
 async function getFigures() { //pedimos datos al servidor
-
-    const result = await fetch("http://localhost:3000/users")
+    const result = await fetch("http://localhost:3000/figures")
     const data= await result.json() 
+    console.log(data) 
     return data
 }
 
 let sectionTag = document.getElementById("figures-list")
 
 async function printFigures () {
-    let Figures = await getFigures()
-    console.log(Figures)
-    Figures.map (Figure => {
+    let figures = await getFigures()
+    console.log(figures)
+    figures.map (figure => {
         sectionTag.innerHTML +=
-        `<h3>${Figure.name}</h3>
-        <p>${Figure.birthdate}</p>
-        <p>${Figure.dateofdeath}</p>
-        <p>${Figure.profession}</p>
-        <p>${Figure.masterpiece}</p>
-        <button onclick="deleteFigure('${Figure.id}')">Delete</button>` 
+        `<h3>${figure.name}</h3>
+        <p>${figure.birthdate}</p>
+        <p>${figure.dateofdeath}</p>
+        <p>${figure.profession}</p>
+        <p>${figure.masterpiece}</p>
+        <button onclick="deleteFigure('${figure.id}')">Delete</button>` 
     })
 }
